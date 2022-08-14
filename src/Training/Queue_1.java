b2d6e5e34b49d4dc1f60fb2a99e569edad5f5d6a
taekgyu <taekgyu0602@gmail.com>
2022-08-14 20:48:06 +0900

    Queue 라이브러리

---
+++ src/Training/Queue_1.java
@@ -0,0 +1,51 @@
+package Training;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queue_1 {
+    public static void main(String[] args) {
+        Queue<Integer> queue1 = new LinkedList<>();
+        Queue<String> queue2 = new LinkedList<>();
+
+        // 값 추가 add, offer
+        queue1.add(1);
+        queue1.add(2);
+        queue1.add(3);
+        queue1.offer(4);
+
+        System.out.println("queue1 : "+ queue1);
+
+        // 값 삭제 poll, remove
+        //queue1.poll();  // 첫 번째 값을 반환하고 제거 비어있다면 null
+        System.out.println(queue1.poll());
+        System.out.println(queue1.remove());
+        System.out.println("queue1 : "+ queue1);
+
+        queue1.clear(); // queue 초기화
+        System.out.println("clear 후 queue : " + queue1);
+
+        queue1.add(1);
+        queue1.add(2);
+        queue1.add(3);
+        queue1.add(4);
+        System.out.println(queue1.peek());  // queue 에서 가장 먼저 들어간 값 출력
+
+        if(queue1.isEmpty()){
+            System.out.println("queue1 이 비어있습니다.");
+        } else{
+            System.out.println("queue1 이 비어있지 않습니다.");
+        }
+        
+        queue1.clear();
+        System.out.println("queue1.clear() 후");
+        if(queue1.isEmpty()){
+            System.out.println("queue1 이 비어있습니다.");
+        } else{
+            System.out.println("queue1 이 비어있지 않습니다.");
+        }
+
+        // size
+        System.out.println(queue1.size());
+    }
+}
