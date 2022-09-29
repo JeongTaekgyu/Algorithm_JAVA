e028934c8b2d109c2c259773f030e547560f67eb
taekgyu <taekgyu0602@gmail.com>
2022-09-29 16:08:13 +0900

    그리디 - 문서 검색

---
+++ src/Baekjoon/_1543_문서_검색.java
@@ -0,0 +1,37 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class _1543_문서_검색 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //선언
+        String document = br.readLine();
+        String search = br.readLine();
+        int cnt = 0;
+
+        document = document.replaceAll(search,"1");
+
+        for(int i = 0; i < document.length(); i++){
+            if(document.charAt(i) == '1')
+                cnt++;
+        }
+
+        // 시간 초과 버전
+        /*int ind = 0;
+        for(int i = 0; i <= document.length() - search.length(); i= search.length()+ind ){
+            ind = document.indexOf(search, i);
+            if(ind != -1)
+                cnt++;
+//            System.out.println("있어? : "+ ind);
+            if(i == 0)
+                i += ind;
+        }*/
+
+        System.out.println(cnt);
+    }
+}
